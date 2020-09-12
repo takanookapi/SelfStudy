153fbf158a347148978f6340e649d8e6bfc51dfc
takanookapi <takdanielu@yahoo.co.jp>
2020-09-12 15:54:59 +0900

    パスワード表示非表示修正

--- app/javascript/packs/security.js
+++ app/javascript/packs/security.js
@@ -8,24 +8,20 @@
 //   alert(e.messasge);
 // }
 
-document.addEventListener("DOMContentLoaded", function(){
-  const password = document.getElementById('user_password');
-  const passwordCheck = document.getElementById('user_password_reveal');
-    passwordCheck.addEventListener('change', function() {
-      if (passwordCheck.checked) {
-        password.setAttribute('type', 'text');
-      } else {
-        password.setAttribute('type', 'password');
-      }
-  });
-}, false);
 
-// // $(document).on('turbolinks:load', 
-// $("user_password_reveal").change(function(){
-//   if($(this).prop('checked')){
-//     $('user_password').attr('type','text');
-//   }else{
-//     $('user_password').attr('type','password');
-//   }
-// });
-// });
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   const password = document.getElementById('user_password');
+//   const passwordCheck = document.getElementById('user_password_reveal');
+//   if(passwordCheck != null) {
+//     passwordCheck.addEventListener('change', function() {
+//       if (passwordCheck.checked) {
+//         password.setAttribute('type', 'text');
+//       } else {
+//         password.setAttribute('type', 'password');
+//       }
+//   });
+// }, false);
