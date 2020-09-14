926a643ee22c7befae1f8554837cd4d76815ee60
takanookapi <takdanielu@yahoo.co.jp>
2020-09-14 13:46:58 +0900

    エラーバンドリング色変更＋ダークモード 搭載

--- app/javascript/packs/security.js
+++ app/javascript/packs/security.js
@@ -25,3 +25,28 @@
 //       }
 //   });
 // }, false);
+
+
+//   // チェックした時の挙動
+//   var btn_b = document.querySelector("#btn-mode");
+//   var footer = document.getElementById('dark_light_change2');
+//   // var header = document.document.getElementsByClassName('containers');
+//   btn_b.addEventListener("change", () => {
+//     if (btn_b.checked == true) {
+// // ダークモード
+//   document.body.classList.remove("light-theme");
+//   document.body.classList.add("dark-theme");
+//   footer.classList.remove("light-theme");
+//   footer.classList.add("dark-theme");
+//   // header.classList.remove("light-theme");
+//   // header.classList.add("dark-theme");
+//     } else {
+// // ライトモード
+//   document.body.classList.remove("dark-theme");
+//   document.body.classList.add("light-theme");
+//   footer.classList.remove("dark-theme");
+//   footer.classList.add("light-theme");
+//   // header.classList.remove("dark-theme");
+//   // header.classList.add("light-theme");
+// }
+// });
