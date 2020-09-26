13c70cfc9725d31d7aba052118424eff238a8dba
takanookapi <takdanielu@yahoo.co.jp>
2020-09-26 23:06:49 +0900

    Heroku修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-   
+    username == ENV["STUDY"] && password == ENV["STUDY_password"]
     end
   end
 
