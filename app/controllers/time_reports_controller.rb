c1cf60f8f81e5a13fd4c80d6a7a701d1427059a8
takanookapi <takdanielu@yahoo.co.jp>
2020-09-26 21:43:57 +0900

    current_user修正

--- app/controllers/time_reports_controller.rb
+++ app/controllers/time_reports_controller.rb
@@ -17,11 +17,11 @@ class TimeReportsController < ApplicationController
 
   def create
     @time_report = TimeReport.new(study_datefiled_time_params)
-    @user = User.find(current_user.id)
+    @current_user = User.find(current_user.id)
     @current_user.sum_time += @time_report.study_time
     @current_user.exp_point += @time_report.exp.to_i
     @current_user.level = @current_user.exp_point.to_i / 30
-    @current_user.update(exp_point: @current_user.exp_point, level: @user.level)
+    @current_user.update(exp_point: @current_user.exp_point, level: @current_user.level)
     if @time_report.save
       redirect_to root_path
     else
