ca08c0d32ea3426260cf14dbe7f4d4138e623b7b
takanookapi <takdanielu@yahoo.co.jp>
2020-10-20 00:56:29 +0900

    アカウント編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'time_reports#index'
 
+  resources :users, only: [:edit, :update]
   resources :time_reports do
     resources :comments, only: [:create, :destroy]
   end
